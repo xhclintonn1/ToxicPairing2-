b325a6437b5b84c1c4606c1e254140133e77249c
xhclint254 <xhclintonyt@gmail.com>
2025-05-16 20:45:03 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,65 +1,75 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router()
-const pino = require("pino");
+const path = require('path');
+let router = express.Router();
+const pino = require('pino');
 const {
-    default: Mbuvi_Tech,
+    default: makeWASocket,
     useMultiFileAuthState,
     delay,
-    makeCacheableSignalKeyStore,
-    Browsers
-} = require("maher-zubair-baileys");
+    Browsers,
+    fetchLatestBaileysVersion,
+    makeCacheableSignalKeyStore
+} = require('@whiskeysockets/baileys');
 
-function removeFile(FilePath){
-    if(!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true })
- };
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function MBUVI_MD_PAIR_CODE() {
-        const {
-            state,
-            saveCreds
-        } = await useMultiFileAuthState('./temp/'+id)
-     try {
-            let Pair_Code_By_Mbuvi_Tech = Mbuvi_Tech({
+
+    async function MBUVI_MD_PAIR_CODE() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let Pair_Code_By_Mbuvi_Tech = makeWASocket({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
                 },
                 printQRInTerminal: false,
-                logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Chrome (Ubuntu)", "Chrome (Linux)", "Chrome (MacOs)"]
-             });
-             if(!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
-                await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
-            Pair_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
-                const {
-                    connection,
-                    lastDisconnect
-                } = s;
-                if (connection == "open") {
-                await delay(5000);
-                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                await delay(800);
-               let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: '' + b64data });
+                logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
+                browser: Browsers.macOS('Safari'),
+            });
 
-               let MBUVI_MD_TEXT = `
-        𝙎𝙀𝙎𝙎𝙄𝙊𝙉 𝘾𝙊𝙉𝙉𝙀𝘾𝙏𝙀𝘿
-        
-         𝙏𝙤𝙭𝙞𝙘-𝙈𝘿 𝙇𝙤𝙜𝙜𝙚𝙙  
+            if (!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            Pair_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds);
+
+            Pair_Code_By_Mbuvi_Tech.ev.on('connection.update', async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection === 'open') {
+                    await delay(10000);
+                    let link = await pastebin.createPasteFromFile(
+                        path.join(__dirname, `/temp/${id}/creds.json`),
+                        'Toxic-MD Session',
+                        null,
+                        1,
+                        'N'
+                    );
+                    let data = link.replace('https://pastebin.com/', '');
+                    let code = btoa(data);
+                    let words = code.split('');
+                    let ress = words[Math.floor(words.length / 2)];
+                    let sessionCode = code.split(ress).join(ress + '_TOXIC_');
+
+                    let MBUVI_MD_TEXT = `
+𝙎𝙀𝙎𝙎𝙄𝙊𝙉 𝘾𝙊𝙉𝙉𝙀𝘾𝙏𝙀𝘿
+
+𝙏𝙤𝙭𝙞𝙘-𝙈𝘿 𝙇𝙤𝙜𝙜𝙚𝙙  
 
 『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
 > 𝐎𝐰𝐧𝐞𝐫: 
@@ -72,33 +82,36 @@ _https://github.com/xhclintohn/Toxic-v2_
 _https://chat.whatsapp.com/GoXKLVJgTAAC3556FXkfFI_
 
 > 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥:
- _https://whatsapp.com/channel/0029VagJlnG6xCSU2tS1Vz19_
+_https://whatsapp.com/channel/0029VagJlnG6xCSU2tS1Vz19_
  
 > 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦:
- _https://www.instagram.com/xh_clinton_
+_https://www.instagram.com/xh_clinton_
 
+Don't Forget To Give Star and fork My Repo :)`;
 
-Don't Forget To Give Star and fork My Repo :)`
+                    await Pair_Code_By_Mbuvi_Tech.sendMessage(
+                        Pair_Code_By_Mbuvi_Tech.user.id,
+                        { text: `${sessionCode}\n\n${MBUVI_MD_TEXT}` }
+                    );
 
- await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
- 
-
-        await delay(100);
-        await Pair_Code_By_Mbuvi_Tech.ws.close();
-        return await removeFile('./temp/'+id);
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(100);
+                    await Pair_Code_By_Mbuvi_Tech.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
                     MBUVI_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
-            console.log("service restated");
-            await removeFile('./temp/'+id);
-         if(!res.headersSent){
-            await res.send({code:"Service Currently Unavailable"});
-         }
+            console.log('Service restarted:', err);
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: 'Service Currently Unavailable' });
+            }
         }
     }
-    return await MBUVI_MD_PAIR_CODE()
+
+    return await MBUVI_MD_PAIR_CODE();
 });
-module.exports = router
+
+module.exports = router;
