4b47256bbb9cfcb5708de2587aed78798dc4b0c7
xhclint254 <xhclintonyt@gmail.com>
2025-05-28 13:44:11 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require('@whiskeysockets/baileys');
+} = require('baileys-elite');
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
